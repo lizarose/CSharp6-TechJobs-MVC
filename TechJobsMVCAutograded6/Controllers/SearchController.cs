d2b767ba4fd179842c5b59057ed3acd064b6563d
Elizabeth Levinson <lizarose266@gmail.com>
2024-06-07 11:30:28 -0500

    Task 3 Completed and Passing All Tests

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -18,5 +18,22 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        List<Job> jobs = new();
+        if(searchType == "all" || searchTerm == "")
+        {
+            jobs = JobData.FindAll();
+            ViewBag.title = "All Jobs";
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+
+        ViewBag.jobs = jobs;
+        ViewBag.ColumnChoices = ListController.ColumnChoices;
+        return View();
+    }
 }
 
